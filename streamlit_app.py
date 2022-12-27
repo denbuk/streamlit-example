098cb5dded2070099e0ea12d62509e9911d235ec
denbuk <62837235+denbuk@users.noreply.github.com>
2022-12-27 15:50:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import requests as rq
 
 """
 # Welcome to Streamlit!
@@ -38,3 +39,6 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+    
+    response = rq.get("http://api.open-notify.org/astros.json")
+    print(response)
