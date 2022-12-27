eafb0e0778fb303794a52d8e829e2b11ab468910
denbuk <62837235+denbuk@users.noreply.github.com>
2022-12-27 15:14:59 +0000

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,6 @@ import streamlit as st
 import requests
 import json
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-Ahoj O2.
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
 with st.echo(code_location='below'):
     
     url = "https://httpbin.org/post"
@@ -33,5 +19,5 @@ with st.echo(code_location='below'):
     }
  
     response = requests.post(url, headers=headers, json=data)
-    
+
     st.write(response)
