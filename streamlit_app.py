627ce0dea363836b42b5eafa4750a547d4807410
denbuk <62837235+denbuk@users.noreply.github.com>
2022-12-27 16:22:43 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ with st.echo(code_location='below'):
     campaign_option = st.selectbox("Campaign Code (campaign_id)", pd.Series(items_ids))
 
     index = items_ids.index(campaign_option)
+    st.write(index)
     item_id = items_ids[index]
     item_properties = items_list[item_id]['properties']["b"]
 
