100ef92803c2ac3c214b567901d76cce3d4ffd31
denbuk <62837235+denbuk@users.noreply.github.com>
2022-12-27 16:24:46 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,6 @@ with st.echo(code_location='below'):
 
     index = items_ids.index(campaign_option)
     item_id = items_ids[index]
-    item_properties = items_list[item_id]
+    item_properties = items_list[index]['properties']["b"]
 
     title = st.text_input('Campaign Name', item_properties)
